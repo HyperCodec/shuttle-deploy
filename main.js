341565332d49955bcf73f365b1900fe8831f72d5
HyperCodec <72839119+HyperCodec@users.noreply.github.com>
2023-08-03 14:41:14 -0400

    add path input

--- main.js
+++ main.js
@@ -6,6 +6,11 @@ async function run() {
    
     const apiKey = core.getInput("api_key", { required: true });
     const name = core.getInput("project_name", { required: true });
+    const path = core.getInput("path");
+
+    if(path !== null) {
+        await exec("cd", [path]);
+    }
 
     core.info("Installing Shuttle");
     await exec("cargo install cargo-shuttle");
